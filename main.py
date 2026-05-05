4c51bc1691d389cd7b083dc3a04760b75bf5bb9a
dhyanp9908 <dhyanp9908@gmail.com>
2026-05-05 12:36:54 +1200

    Creating class for home page

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("500x500")
+window.title("Get It Done")
+
+window.config(background="#8babd4")
+
+class Homepage(tk.Frame):
+    def__init__(self, parent):
+
+window.mainloop()#place window on computer screen, listen for events
