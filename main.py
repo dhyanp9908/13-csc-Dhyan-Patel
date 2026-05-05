73bc6a9c4a7e3617d064925e7bc42e5cf4374c6c
dhyanp9908 <dhyanp9908@gmail.com>
2026-05-06 11:10:46 +1200

    creating title

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk, messagebox
+import json
+import os
+from datetime import datetime
+import threading
+import time
 
-window = Tk()
-window.geometry("500x500")
-window.title("Get It Done")
 
-window.config(background="#8babd4")
 
-class Homepage(tk.Frame):
-    def__init__(self, parent):
+
+
+#color plette
+COLORS = {
+    'Baby_Blue': '#8babd4',
+    'Black': '#000000',
+}
+
+class HomePage(tk.Frame):
+    def __init__(self, parent):
+         super().__init__(parent, bg=COLORS['Baby_Blue'])
+
+         #Title
+         title = tk.Label(self, text='Get It Done',
+             font=('Cooper Hewitt', 64, 'bold'),
+             bg=COLORS['#Baby_Blue'],
+             fg=COLORS['Black'])
+         title.pack(pady=(60,5))
+
+window = tk.Tk()
+window.geometry("1000x700")
+window.configure(bg=COLORS['Baby_Blue'])
+
+page = HomePage(window)
+page.pack(fill='both', expand=True)
 
 window.mainloop()#place window on computer screen, listen for events
