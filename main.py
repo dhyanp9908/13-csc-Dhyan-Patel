cd6833a07848bdbbd8c3db582d0d27252821d7d6
dhyanp9908 <dhyanp9908@gmail.com>
2026-05-15 15:00:51 +1200

    creating button

--- main.py
+++ main.py
@@ -11,16 +11,11 @@ label = Label(window,
               font=('Cooper Hewitt',100, 'bold'),
               fg='#000000',
               bg='#8babd4')
-
 label.pack(pady=(60,5))
 
-def click():
-     print("hello")
+button = tkinter.Button(window, text='Get Started', command=window. )
 
 
-button = Button(window, text=('Get Started')) #start button
-button.config(command=click)
-button.config(font=('Canva sans',60,'bold'))
-button.config(bg='#ffffcc')
-button.pack(pady=(200,25))
+
 window.mainloop()#place window on computer screen, listen for events
+
