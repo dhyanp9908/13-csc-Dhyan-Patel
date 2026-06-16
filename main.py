90005cd9ab0a9cec5fa43b8c50e383a29062329b
dhyanp9908 <dhyanp9908@gmail.com>
2026-06-16 12:34:06 +1200

    Creating first page

--- main.py
+++ main.py
@@ -2,24 +2,55 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import datetime
 
-BG = "#8babd4" #Background color
-CARD = "#ffffcc" #Button
-FG = "#000000" #text color
-
-root = tk.Tk()
-root.title("Get It Done")
-root.geometry("900x600")
-root.configure(bg=BG)
-
-page1 = tk.Frame(root, bg=BG)
-page2 = tk.Frame(root, bg=BG)
-page3 = tk.Frame(root, bg=BG)
-page4 = tk.Frame(root, bg=BG)
 
 
 
+BG = "#8babd4"
+CARD = "#ffffcc"
+DARK = "#000000"
+SIDEBAR = "#F5F0c8"
+
+T_TITLE = ("Georgia", 36, "bold")
+T_SUB = ("Georgia", 16, "bold")
+T_BODY = ("Georgia", 13)
+T_BTN = ("Georgia", 14, "bold")
+T_SM = ("Georgia", 11)
 
 
 
-root.mainloop()#place window on computer screen, listen for events
+def btn(parent, label, action, w:int=12):
+    return tk.Button(parent, text=label, command=action,
+                     font=T_BTN, bg=CARD, fg=DARK,
+                     relief="flat", padx=16, pady=8,
+                     width=w, cursor="hand2",
+                     activebackground="#E8E2AA")
+class Page1(tk.Frame):
+    def __init__(self, master:Misc, go_to_page2):
+        super().__init__(master, bg=BG)
+
+    tk.Label(self, text="Get It Done", font=T_TITLE, bg=BG, fg=DARK).pack(pady=(20, 4))
+    tk.Frame(self, bg=DARK, height =2, width=500).pack()
+
+    tk.Label(self, text="")
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Get It Done")
+        self.geometry("900x600")
+        self.configure(bg=BG)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    App().mainloop()
+
+
 
